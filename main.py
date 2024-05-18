0642c6f3c54dc5479f76d79cdba5a1a84e2fa5e7
Marksman <bodnarmark@outlook.com>
2024-05-18 09:45:36 +0200

    enyem sokkal jobb

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-import pandas
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn import metrics
+
+data_dir = 'dataset'
+
+df_train = pd.read_csv(data_dir + '\\Training Data.csv')
+df_test = pd.read_csv(data_dir + '\\Test Data.csv')
+
+
+x_train, x_test, y_train, y_test = train_test_split(df_train.drop('Risk_Flag', axis=1), df_train['Risk_Flag'], test_size=0.2, random_state=109)
+
+# Transforming textual attributes to numerical with LabelEncoder
+for i, dtype in enumerate(x_train.dtypes):
+    if dtype == 'object':
+        le = LabelEncoder()
+        x_train.iloc[:, i] = le.fit_transform(x_train.iloc[:,i])
+        x_test.iloc[:, i] = le.fit_transform(x_test.iloc[:,i])
+
+clf = svm.SVC(kernel='linear', verbose=False)
+
+clf.fit(x_train.iloc[0:100, 0:6], y_train[0:100])
+
+y_pred = clf.predict(x_test.iloc[0:100, 0:6])
+
+print("Accuracy:", metrics.accuracy_score(y_test[0:100], y_pred))
